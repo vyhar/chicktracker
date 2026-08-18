81ce02948760d4e6e47e05afd5ad0b48ba1ddfce
vyhar <justin@vyhar.com>
2026-08-18 08:33:07 -0400

    updated hatch weekley

--- data/hatch_weekly.js
+++ data/hatch_weekly.js
@@ -88,5 +88,130 @@ const HATCH_WEEKLY = [
     breeds:{
       Cornish:{ship:11501, del:2275, pickup:2380},
   }
-}
+},
+
+{
+    date:'06-02',
+    ship:6207,
+    delpu:6165,
+    hpct:68.7,
+    eggs:18000,
+    breeds:{
+      Cornish:{ship:6207, del:4700, pickup:1465}
+    }
+  },
+
+  {
+    date:'06-09',
+    ship:11777,
+    delpu:21149,
+    hpct:82.5,
+    eggs:39600,
+    breeds:{
+      Cornish:{ship:9042, del:3977, pickup:571},
+      Ranger:{ship:2735, del:16300, pickup:301}
+    }
+  },
+
+  {
+    date:'06-16',
+    ship:6009,
+    delpu:9512,
+    hpct:71.9,
+    eggs:21600,
+    breeds:{
+      Cornish:{ship:6009, del:8950, pickup:562}
+    }
+  },
+
+  {
+    date:'06-23',
+    ship:10436,
+    delpu:3898,
+    hpct:66.7,
+    eggs:21504,
+    breeds:{
+      Cornish:{ship:10436, del:1953, pickup:1945}
+    }
+  },
+
+  {
+    date:'06-30',
+    ship:20555,
+    delpu:17007,
+    hpct:87.5,
+    eggs:42837,
+    breeds:{
+      Cornish:{ship:8002, del:7080, pickup:1770},
+      Ranger:{ship:12553, del:6355, pickup:1802}
+    }
+  },
+
+  {
+    date:'07-07',
+    ship:9416,
+    delpu:8480,
+    hpct:82.9,
+    eggs:21600,
+    breeds:{
+      Cornish:{ship:9416, del:7775, pickup:705}
+    }
+  },
+
+  {
+    date:'07-14',
+    ship:13177,
+    delpu:4020,
+    hpct:79.6,
+    eggs:21600,
+    breeds:{
+      Cornish:{ship:13177, del:3035, pickup:985}
+    }
+  },
+
+  {
+    date:'07-21',
+    ship:22073,
+    delpu:14673,
+    hpct:85.2,
+    eggs:43140,
+    breeds:{
+      Cornish:{ship:10528, del:5328, pickup:325},
+      Ranger:{ship:11545, del:9000, pickup:20}
+    }
+  },
+
+  {
+    date:'07-28',
+    ship:8083,
+    delpu:9665,
+    hpct:82.4,
+    eggs:21528,
+    breeds:{
+      Cornish:{ship:8083, del:9200, pickup:465}
+    }
+  },
+
+  {
+    date:'08-04',
+    ship:9500,
+    delpu:6285,
+    hpct:73.6,
+    eggs:21450,
+    breeds:{
+      Cornish:{ship:9500, del:4125, pickup:2160}
+    }
+  },
+  
+  {
+    date:'08-11',
+    ship:8722,
+    delpu:28020,
+    hpct:85.1,
+    eggs:43200,
+    breeds:{
+      Cornish:{ship:8170, del:5990, pickup:2406},
+      Ranger:{ship:552, del:19425, pickup:199}
+    }
+  }
 ];
